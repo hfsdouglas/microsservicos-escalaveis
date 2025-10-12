14e31b39d6fcb1e7b43b0d1e301f46e96e9fc649
Douglas Faria <hfsdouglas@gmail.com>
2025-10-12 13:56:39 -0300

    feat: add rota orders para criação de novas vendas

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 import fastify from "fastify";
+import { eq } from "drizzle-orm";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
 
 import { db } from "../lib/drizzle.ts";
@@ -10,10 +11,32 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-app.get('/hello', () => {
-    return "Hello World"
+app.post('/orders', {
+    schema: {
+        body: z.object({
+            amount: z.coerce.number().transform((n) => n.toFixed(2)), // O PG aceita apenas string para tipos decimais. 
+            clientId: z.uuid()
+        })
+    }
+}, async (request, reply) => {
+    const { amount, clientId } = request.body
+
+    const client = await db.select().from(schema.clients).where(eq(schema.clients.id, clientId))
+
+    if (client.length === 0) {
+        return reply.status(404).send({
+            'message': 'Client not found!'
+        })
+    }
+
+    await db.insert(schema.orders).values({
+        clientId,
+        amount
+    })
+
+    return reply.status(201).send()
 })
 
 app.listen({ port: 3333 }).then(() => {
-    console.log("Server is running on port 3333!")
+    console.log("HTTP Server is running! 🚀")
 })
